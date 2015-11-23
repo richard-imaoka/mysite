c304e06cd3446f9615f3fb04b695f726bf6bbcb3
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2015-11-23 23:50:47 +0900

    some jade testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var fs = require("fs");
+var jade = require("gulp-jade");
+var rename = require("gulp-rename");
+var change = require("gulp-change");
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
+
+gulp.task('jade', function () {
+  var files = fs.readdirSync(".");
+  console.log(files);
+  gulp.src("jade/toc.jade")
+      .pipe(jade({
+        locals: files,
+        data: files,
+        pretty: true
+      }))
+      .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('jadify', function () {
+  gulp.src("html/articles/*.html")
+      .pipe(change(function(content){
+        return this.fname;
+      }))
+      .pipe(rename(function (path) {
+        path.extname = ".jade";
+        return path;
+      }))
+      .pipe(gulp.dest('./dist/'));
+})
